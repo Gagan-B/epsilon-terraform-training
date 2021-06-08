072c8be50eeb2e12d5b9dfedbb29aeb046474db1
Gagan-B <gaganb1998@gmail.com>
2021-06-08 00:03:55 -0700

    adding ebs_volume

--- training-ec2/main.tf
+++ training-ec2/main.tf
@@ -8,3 +8,14 @@ resource "aws_instance" "gagan_ec2" {
   instance_type = var.ec2_instance_type
   subnet_id     = var.public_subnet_id
 }
+
+resource "aws_ebs_volume" "gagan_ebs_volume" {
+  availability_zone = "us-east-1a"
+  size              = 1
+}
+
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.gagan_ebs_volume.id
+  instance_id = aws_instance.gagan_ec2.id
+}
