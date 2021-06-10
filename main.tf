33395c2705c644503d6b5ba606e405ce02d42969
Gagan-B <gaganb1998@gmail.com>
2021-06-09 23:35:07 -0700

    introducing modules

---
+++ main.tf
@@ -0,0 +1,14 @@
+terraform {
+  backend "s3" {
+    bucket = "gagan-terraform-training"
+    key    = "terraform_module.tfstate"
+    region = "us-east-1"
+  }
+}
+
+module "ec2" {
+  source     = "./modules/ec2"
+  #source     = "git::git@github.com:manishsethi/xebia-epsilon-terraform-training.git//modules/ec2?ref=v0.1.1"
+  aws_region = "us-east-1"
+
+}
